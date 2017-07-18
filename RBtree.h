6ec0385099905890d36fab46897b538fcdcf1852
hadman <falaza.studio@gmail.com>
2017-07-19 09:51:24 +1000

    final

--- RBtree.h
+++ RBtree.h
@@ -7,9 +7,9 @@
 
 #endif //RED_BLACK_TREE_CLASS_RBTREE_H
 
-// 1) корень дерева - черный
+// 1) корень дерева - черный (0)
 // 2) каждый лист - черный
-// 3) если узел красный - оба сына черные
+// 3) если узел красный (1) - оба сына черные
 // 4) для каждого узла все простые пути от него до листьев,
 // являющихся его потомками, содержат одинаковое кол-во черных  узлов
 
@@ -23,63 +23,46 @@ struct node{
 
 class RBtree {
 private:
-    node* root;
     node* nil;                  // ограничитель
-public:
-    RBtree(){
-        root = new node;
-        nil = new node;
-        nil->color = 'B';
-    }
-    void RBInsert(int key){            // добавить узел
-        node* y = this->nil;
-        node* x = this->root;
-        node* newNode = new node;
-        newNode->key = key;
+    void rightRotate(node* x){
+        node* y = x->childLeft;
+        x->childLeft = y->childRight;
 
-        while (x != this->nil){
-            y = x;
-            if ( newNode->key < x->key){
-                x = x->childLeft;
+        if (y->childRight != nil){
+            y->childRight->p = x;
+        }
+
+        y->p = x->p;
+
+        if(x->p == nil)
+        {
+            root = y;
+        } else {
+            if (x == x->p->childRight){
+                x->p->childRight = y;
             } else{
-                x = x->childRight;
+                x->p->childLeft = y;
             }
         }
-//
-//        newNode->p = y;
-//
-//        if( y == this->nil){
-//            this->root = newNode;
-//        } else{
-//            if(newNode->key < y->key){
-//                y->childLeft = newNode;
-//            } else{
-//                y->childRight = newNode;
-//            }
-//        }
-//        newNode->childLeft = this->nil;
-//        newNode->childRight = this->nil;
-//        newNode->color = 'R';
-//        this->RBInsertFixup(newNode);
-    }
-
-    void rightRotate(node* x){
-
+        y->childRight = x;
+        if(x != nil){
+            x->p = y;
+        }
     }
 
     void leftRotate(node* x){
         node* y = x->childRight;
         x->childRight = y->childLeft;
 
-        if (y->childLeft != this->nil){
+        if (y->childLeft != nil){
             y->childLeft->p = x;
         }
 
-        y->p = x;
+        y->p = x->p;
 
-        if(x->p == this->root)
+        if(x->p == nil)
         {
-            this->root = y;
+            root = y;
         } else {
             if (x == x->p->childLeft){
                 x->p->childLeft = y;
@@ -87,60 +70,237 @@ public:
                 x->p->childRight = y;
             }
         }
-        x->p = y;
+        y->childLeft = x;
+        if(x != nil){
+            x->p = y;
+        }
     }
 
-    int bh(node* x){            // черная высота узла
-
-    }
 
     void RBInsertFixup(node* z){
-        while (z->p->color == 'R'){
-            if (z->p == z->p->p->childLeft){
-                node* y = z->p->p->childRight;
-                if(y->color == 'R'){
-                    z->p->color = 'B';
-                    y->color = 'B';
-                    z->p->p->color = 'R';
-                    z = z->p->p;
+        while (z != root && z->p->color == '1') {
+            /* we have a violation */
+            if (z->p == z->p->p->childLeft) {
+                node *y = z->p->p->childRight; // временный дядя справа
+                if (y->color == '1') {
+
+                    /* uncle is RED */
+                    z->p->color = '0';
+                    y->color = '0';
+                    z->p->p->color = '1';
+                    z = z->p->p; // переход в деда
                 } else {
-                    if(z == z->p->childRight){
+
+                    /* uncle is BLACK */
+                    if (z == z->p->childRight) {
+                        /* make x a left child */
                         z = z->p;
                         leftRotate(z);
                     }
-                    z->p->color = 'B';
-                    z->p->p->color = 'R';
+
+                    /* recolor and rotate */
+                    z->p->color = '0';
+                    z->p->p->color = '1';
                     rightRotate(z->p->p);
                 }
-            } else{
-                node* y = z->p->p->childLeft;
-                if(y->color == 'R'){
-                    z->p->color = 'B';
-                    y->color = 'B';
-                    z->p->p->color = 'R';
+            } else {
+
+                /* mirror image of above code */
+                node *y = z->p->p->childLeft;
+                if (y->color == '1') {
+
+                    /* uncle is RED */
+                    z->p->color = '0';
+                    y->color = '0';
+                    z->p->p->color = '1';
                     z = z->p->p;
                 } else {
-                    if(z == z->p->childLeft){
+
+                    /* uncle is BLACK */
+                    if (z == z->p->childLeft) {
                         z = z->p;
-                        leftRotate(z);
+                        rightRotate(z);
                     }
-                    z->p->color = 'B';
-                    z->p->p->color = 'R';
-                    rightRotate(z->p->p);
+                    z->p->color = '0';
+                    z->p->p->color = '1';
+                    leftRotate(z->p->p);
                 }
             }
         }
-        root->color = 'B';
+        root->color = '0';
     }
 
-    void printRBTree(node* z, int h){
-        if(z != this->nil){
-            printRBTree(z->childLeft, h+1);
-            for (int i = 1; i < h; ++i) {
-                std::cout << " ";
+    void deleteFixup(node* x){
+        /*************************************
+    *  maintain Red-Black tree balance  *
+    *  after deleting node x            *
+    *************************************/
+
+        while (x != root && x->color == '0') {
+            if (x == x->p->childLeft) {
+                node *w = x->p->childRight;
+                if (w->color == '1') {
+                    w->color = '0';
+                    x->p->color = '1';
+                    leftRotate (x->p);
+                    w = x->p->childRight;
+                }
+                if (w->childLeft->color == '0' && w->childRight->color == '0') {
+                    w->color = '1';
+                    x = x->p;
+                } else {
+                    if (w->childRight->color == '0') {
+                        w->childLeft->color = '0';
+                        w->color = '1';
+                        rightRotate (w);
+                        w = x->p->childRight;
+                    }
+                    w->color = x->p->color;
+                    x->p->color = '0';
+                    w->childRight->color = '0';
+                    leftRotate (x->p);
+                    x = root;
+                }
+            } else {
+                node *w = x->p->childLeft;
+                if (w->color == '1') {
+                    w->color = '0';
+                    x->p->color = '1';
+                    rightRotate (x->p);
+                    w = x->p->childLeft;
+                }
+                if (w->childRight->color == '0' && w->childLeft->color == '0') {
+                    w->color = '1';
+                    x = x->p;
+                } else {
+                    if (w->childLeft->color == '0') {
+                        w->childRight->color = '0';
+                        w->color = '1';
+                        leftRotate (w);
+                        w = x->p->childLeft;
+                    }
+                    w->color = x->p->color;
+                    x->p->color = '0';
+                    w->childLeft->color = '0';
+                    rightRotate (x->p);
+                    x = root;
+                }
             }
-            std::cout << z->key;
-            printRBTree(z->childRight, h+1);
+        }
+        x->color = '0';
+    }
+    node *findNode(int data) {
+
+        /*******************************
+         *  find node containing data  *
+         *******************************/
+
+        node *current = root;
+        while(current != nil)
+            if(data == current->key)
+                return (current);
+            else
+                current = (data < current->key) ?
+                          current->childLeft : current->childRight;
+        return(0);
+    }
+
+
+public:
+    node* root;
+
+    RBtree(){
+        nil = new node;
+        nil->color = '0';
+        root = nil;
+    }
+    node* RBInsert(int key){            // добавить узел
+        node* curr, *prev, *x;
+
+        curr = root;
+        prev = nil;
+        while (curr != nil){
+            if(key == curr->key) return curr;
+            prev = curr;
+            curr = (key < curr->key) ? curr->childLeft : curr->childRight;
+        }
+
+        x = new node();
+        x->key = key;
+        x->p = prev;
+        x->childLeft = nil;
+        x->childRight = nil;
+        x->color = '1';
+
+        if (prev != nil){
+            if(key < prev->key){
+                prev->childLeft = x;
+            } else{
+                prev->childRight = x;
+            }
+        } else{
+            root = x;
+        }
+        RBInsertFixup(x);
+        return (x);
+    }
+    void RBdelete(int key){
+        node *z, *x, *y;
+        z = findNode(key);
+
+        /*****************************
+         *  delete node z from tree  *
+         *****************************/
+
+        if (z == nil || z == 0) return;
+
+
+        if (z->childLeft == nil || z->childRight == nil) {
+            /* y has a NIL node as a child */
+            y = z;
+        } else {
+            /* find tree successor with a NIL node as a child */
+            y = z->childRight;
+            while (y->childLeft != nil) y = y->childLeft;
+        }
+
+        /* x is y's only child */
+        if (y->childLeft != nil)
+            x = y->childLeft;
+        else
+            x = y->childRight;
+
+        /* remove y from the parent chain */
+        x->p = y->p;
+        if(y->p == nil){
+            root = x;
+        }else{
+            if (y == y->p->childLeft)
+                y->p->childLeft = x;
+            else
+                y->p->childRight = x;
+        }
+
+        if (y != z) z->key = y->key;
+
+
+        if (y->color == '0')
+            deleteFixup (x);
+
+        delete y;
+    }
+
+
+
+    void printRBTree(node* z, int h){
+
+        if(z != nil)
+        {
+            printRBTree(z->childRight,h + 1);
+            for(int i = 0;i< h;i++) std::cout<<"     ";
+            std::cout << z->key << "(" << z->color << ")" << std::endl;
+            std::cout << std::endl;
+            printRBTree(z->childLeft,h + 1);
         }
     }
 };
--- main.cpp
+++ main.cpp
@@ -5,7 +5,33 @@ using namespace std;
 
 int main() {
     RBtree* tree = new RBtree();
-//    tree->RBInsert(1);
-    std::cout << "Hello, Tree!" << std::endl;
+
+    tree->RBInsert(92);
+    tree->RBInsert(57);
+    tree->RBInsert(79);
+    tree->RBInsert(66);
+    tree->RBInsert(89);
+    tree->RBInsert(46);
+    tree->RBInsert(77);
+    tree->RBInsert(72);
+    tree->RBInsert(84);
+    tree->RBInsert(19);
+    tree->RBInsert(56);
+    tree->RBInsert(97);
+    tree->RBInsert(3);
+    tree->printRBTree(tree->root, 0);
+
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << "*******" << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    tree->RBdelete(79);
+    tree->printRBTree(tree->root, 0);
+
+
+
     return 0;
 }
